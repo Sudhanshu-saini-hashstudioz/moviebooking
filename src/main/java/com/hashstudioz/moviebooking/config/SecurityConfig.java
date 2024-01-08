e4c9b3f28e31811bacd4bd339af20910a4d94b69
Sudhanshu-saini-hashstudioz <155941662+Sudhanshu-saini-hashstudioz@users.noreply.github.com>
2024-01-08 12:43:43 +0530

    Update SecurityConfig.java

--- src/main/java/com/hashstudioz/moviebooking/config/SecurityConfig.java
+++ src/main/java/com/hashstudioz/moviebooking/config/SecurityConfig.java
@@ -9,28 +9,27 @@ import org.springframework.security.authentication.dao.DaoAuthenticationProvider
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 import com.hashstudioz.moviebooking.entities.classenum.Role;
 import com.hashstudioz.moviebooking.services.UserService;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import lombok.RequiredArgsConstructor;
 
 @Configuration
 @EnableWebSecurity
-@EnableWebMvc
+//@EnableWebMvc
 @RequiredArgsConstructor
+@OpenAPIDefinition
 public class SecurityConfig {
 	
 	public static final String[] PUBLIC_URLS = {
-			"/v3/api-docs",
-			"/v2/api-docs",
+			"/v3/api-docs/**", "/v2/api-docs/**",
 			"/swagger-resources/**",
 			"/swagger-ui/**",
 			"/webjars/**"
@@ -44,7 +43,7 @@ public class SecurityConfig {
 
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-		http.csrf(AbstractHttpConfigurer::disable)
+		http
 		.authorizeHttpRequests(request -> request
 				.requestMatchers("/api/v1/auth/**").permitAll()
 				.requestMatchers(PUBLIC_URLS).permitAll()
